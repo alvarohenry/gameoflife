810860f31bf3b76b7279fda06930a694ee1fb0dc
Alvaro H. M. Aliaga <alvarohenry@gmail.com>
2014-02-03 06:58:40 -0500

    gameOfLife directory

---
+++ gameOfLife/gameLife.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# gameLife.py
+# 
+# Alumno: Alvaro Henry Mamani Aliaga
+#
+
+import gi.repository import Gtk
+
+class GameLife(Gtk.Window):
+	def __init__(self):
+		Gtk.Window.__init__(self, title="Conway's Game if Life")
+		
+
+		
+
